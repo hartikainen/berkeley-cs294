d7fc211596e0e4108df1bd14c6d62c96e4e0dfac
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-06-28 21:31:12 -0700

    Add behavioral cloning plot for vis.py

--- homework1/vis.py
+++ homework1/vis.py
@@ -1,8 +1,10 @@
+from itertools import groupby
+import json
+import argparse
+
 import matplotlib
 # matplotlib.use(args.mpl_backend)
 import matplotlib.pyplot as plt
-import json
-import argparse
 
 import numpy as np
 
@@ -22,12 +24,77 @@ def parse_args():
                         type=str,
                         help="Path to the behavioral cloning results file")
 
+    parser.add_argument("--num_rollouts",
+                        type=int,
+                        default=20,
+                        help="Number of expert roll outs")
+
+    parser.add_argument("--dagger_N",
+                        type=int,
+                        default=10,
+                        help=("Number of dagger iterations."))
+
     args = vars(parser.parse_args())
 
     return args
 
 
-def plot_dagger(dagger_results, bc_results, expert_results):
+def plot_behavioral_cloning(bc_results):
+    bc_results = sorted(
+        sorted(bc_results, key=lambda x: x['num_rollouts']),
+        key=lambda x: x['env']
+    )
+
+
+    for env, results in groupby(bc_results, lambda r: r['env']):
+        rollouts, mean_returns, std_returns = zip(*[
+            (r['num_rollouts'], np.mean(r['returns']), np.std(r['returns']))
+            for r in results
+        ])
+
+        plt.errorbar(rollouts, mean_returns, yerr=std_returns,
+                     fmt='-o', capsize=4, elinewidth=2, label=env)
+
+    plt.legend(loc="upper right")
+    plt.xlabel("mean returns", fontsize=16)
+    plt.ylabel("number of rollouts", fontsize=16)
+    plt.suptitle("Behavioral cloning mean rewards against expert rollouts",
+                 fontsize=20)
+
+    plt.show()
+
+
+def filter_data(dagger_results, bc_results, expert_results):
+    """ Filters and sorts the data to be used for visualization
+
+    Input:
+    - dagger_results, bc_results, expert_results: Arrays of containing results
+      for the runs.
+    - num_rollouts: number of rollouts to filter the data based on
+    - dagger_N: number of dagger iterations to filter the data based on
+
+    Returns:
+    - dagger_results, expert_results, bc_results: filtered lists of same
+      length, and each sorted by environment name
+    """
+    dagger_results = filter(lambda res: (res['num_rollouts'] == num_rollouts
+                                         and res['dagger_N'] == dagger_N),
+                            dagger_results)
+    dagger_results = sorted(dagger_results, key=lambda res: res['env'])
+
+    expert_results = filter(lambda res: res['num_rollouts'] == num_rollouts,
+                            expert_results)
+    expert_results = sorted(expert_results, key=lambda res: res['envname'])
+    bc_results = filter(lambda res: res['num_rollouts'] == num_rollouts,
+
+                        bc_results)
+    bc_results = sorted(bc_results, key=lambda res: res['env'])
+
+    return dagger_results, expert_results, bc_results
+
+
+def plot_dagger(dagger_results, bc_results, expert_results,
+                num_rollouts, dagger_N):
     """ DAgger results plot with comparison against BC and expert results
 
     Input:
@@ -38,6 +105,12 @@ def plot_dagger(dagger_results, bc_results, expert_results):
     - expert_results: Array of length N, containing results for expert,
       correspoding to the dagger_results
     """
+    dagger_results, bc_results, expert_results = filter_data(dagger_results,
+                                                             bc_results,
+                                                             expert_results,
+                                                             num_rollouts,
+                                                             dagger_N)
+
     for dagger_result, bc_result, expert_result in zip(dagger_results,
                                                         bc_results,
                                                         expert_results):
@@ -68,20 +141,30 @@ def plot_dagger(dagger_results, bc_results, expert_results):
 
     return
 
+
 if __name__ == "__main__":
     args = parse_args()
+    num_rollouts = args['num_rollouts']
+    dagger_N = args['dagger_N']
+
     with open(args['dagger_results'], 'r') as f:
         dagger_results = json.load(f)
-    dagger_results = sorted(dagger_results, key=lambda res: res['env'])
 
     with open(args['expert_results'], 'r') as f:
         expert_results = json.load(f)
-    expert_results = filter(lambda res: res['num_rollouts'] == 10, expert_results)
-    expert_results = sorted(expert_results, key=lambda res: res['envname'])
 
     with open(args['bc_results'], 'r') as f:
         bc_results = json.load(f)
-    bc_results = filter(lambda res: res['num_rollouts'] == 10, bc_results)
-    bc_results = sorted(bc_results, key=lambda res: res['env'])
 
-    plot_dagger(dagger_results, bc_results, expert_results)
+    plot_behavioral_cloning(bc_results)
+
+    assert(all((dr['env'] == bcr['env'] == er['envname']
+                and (dr['num_rollouts']
+                     == bcr['num_rollouts']
+                     == er['num_rollouts']))
+               for dr, bcr, er in zip(dagger_results,
+                                      bc_results,
+                                      expert_results)))
+
+    plot_dagger(dagger_results, bc_results, expert_results,
+                num_rollouts, dagger_N)
